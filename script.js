2423ea5893292fcad064b821d004f85e1e83dbdf
Fahad Rafiq <73123765+Fahad-Rafiq06@users.noreply.github.com>
2023-11-30 20:29:26 +0500

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,7 @@
 // }
 
 
-// game to guess right number 
+// game to guess the right number 
 
 // let gamenum = 68;
 
@@ -45,3 +45,30 @@
 // let output = `@${username}${username.length}`;
 
 // console.log(output)
+
+
+// Checking the difference between for-of and for-in loop
+
+let heroes = ["Ironman" , "Captain America", "Spider-man", "Thor", "Hulk"];
+let cities = ["Karachi", "islamabad", "lahore", "quetta"];
+
+for (let hero of heroes){ //prints value directly that's why used well for strings and arrays
+    console.log(hero)
+}
+
+for (let city in cities){ //prints keys/indices directly that's why works well for objects
+    console.log(cities[city])
+}
+
+// finding average of marks
+
+let marks = [85,97,44,37,76,60];
+let sum = 0;
+for (let i=0; i < marks.length; i++){
+    // console.log(sum+=marks[i])
+    sum+=marks[i]
+}
+
+let avg = sum/marks.length;
+
+console.log(`the average marks of class = ${avg}`)
